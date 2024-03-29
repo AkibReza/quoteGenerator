9b4d632f55b9544e871205b9ede1e8da5c750737
Akib Reza <akib.reza23@gmail.com>
2024-03-29 15:28:03 +0600

    potential bugfix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./quoteGenerator",
+  base: "https://akibreza.github.io/quoteGenerator/",
   plugins: [react()],
 });
